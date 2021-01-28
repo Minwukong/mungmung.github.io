cc88a56b348ecef4246090a36522f64f6aa67829
KangYeJin <77770522+kyjin8@users.noreply.github.com>
2021-01-28 17:05:18 +0900

    db연습
    db연습 테스트 업로드

---
+++ 20210127_dbtest_kyj/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>
+    no meaning sentence
+</body>
+</html>
