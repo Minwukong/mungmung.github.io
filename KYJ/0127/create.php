9cc498dc0ba799ead785bdc5a002b81aa950d315
KangYeJin <kyjin8@naver.com>
2021-01-29 15:17:27 +0900

    폴더명 변경
    폴더명 변경 및 날짜별 폴더 생성

---
+++ KYJ/0127/create.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <form action="process_create.php" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p><textarea name="description" placeholder="description"></textarea></p>
+        <p><input type="submit"></p>
+    </form>
+</body>
+</html>
