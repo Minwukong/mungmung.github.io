689b8af9766258dcbc6066e8458d5f84274b1b88
KangYeJin <kyjin8@naver.com>
2021-01-29 15:59:13 +0900

    join적용전
    .

---
+++ KYJ/0128/before_join/index.php
@@ -0,0 +1,60 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  'qmffhrcpdls',
+  'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)){ 
+    // $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    $escaped_title = htmlspecialchars($row['title']);
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
+}
+$article = array(
+    'title'=>'Welcome',
+    'description'=>'Hello, web'
+);
+$update_link = '';
+$delete_link = '';
+if(isset($_GET['id'])) {
+    $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+    // $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    // $article = array(
+    //     'title'=>$row['title'],
+    //     'description'=>$row['description']
+    // );
+    $article['title'] = htmlspecialchars($row['title']);
+    $article['description'] = htmlspecialchars($row['description']);
+
+    $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+    $delete_link = '
+    <form action="process_delete.php" method="post">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type="submit" value="delete">
+    </form>
+    ';
+}
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB</title>
+</head>
+<body>
+<h1><a href="index.php">WEB</a></h1>
+    <ol><?=$list;?></ol>
+    <a href="create.php">create</a>
+    <?=$update_link?>
+    <?=$delete_link?>
+    <h2><?=$article['title']?></h2>
+    <?=$article['description']?>
+</body>
+</html>
