f4cd7088f1a0e8fe679ef1ab54a16e68af96699f
KangYeJin <kyjin8@naver.com>
2021-02-02 12:02:35 +0900

    db 이미지 불러옴, 이미지 위치 조정
    1. db에 닷홈 이미지 경로 넣어놓고 출력
    ㄴ 기본 이미지: 고양이, 테스트 이미지: 메인배경
    2. postImg.js로 이미지 가운데가 사이즈 맞춰 보이게 조정

---
+++ KYJ/0202/process_update.php
@@ -0,0 +1,41 @@
+<?php
+require_once('./view/php_connect_root.php');
+// require_once('./view/php_connect_h.php');
+
+    settype($_POST['id'], 'integer');
+    $filtered = array(
+      'id'=>mysqli_real_escape_string($conn, $_POST['id']),
+      'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+      'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+  );
+
+$sql = "
+    UPDATE topic
+        SET
+            title = '{$filtered['title']}',
+            description = '{$filtered['description']}'
+        WHERE
+            id = {$filtered['id']}
+";
+$result = mysqli_query($conn, $sql);
+
+    require_once('./view/html_top.php');
+?>
+    </div>
+    <div id="Wrap" class="wrap_posts">
+        <h1><a class="main_title" href="index.php">게시판</a></h1>
+        <div class="show_box">
+            <div class="cru_box">
+            <?php
+            if($result === false){
+                echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.';
+                error_log(mysqli_error($conn));
+            } else {
+                echo '성공했습니다.<br><br> <a href="index.php">돌아가기</a>';
+            }
+            ?>
+            </div>
+        </div>
+<?php
+    require_once('./view/bottom.php');
+?>
---
+++ KYJ/0202/view/bottom.php
@@ -0,0 +1,5 @@
+</div>
+<script src="./js/slide.js"></script>
+<script src="./js/postImg.js"></script>
+</body>
+</html>
