6095a9f97ba4c564f18fd7cc6958605b163bd47b
iamnothalim <iamnothalim@naver.com>
2021-02-05 02:25:00 +0900

    로그인 세션 구현
    로그인 하면 메뉴바 로그인 버튼 로그아웃으로 바뀜

---
+++ SHL/0205/view/php_connect_root.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  'qmffhrcpdls',
+  // 'opentutorials'
+  'first'
+);
---
+++ SHL/0205/view/php_top.php
@@ -0,0 +1,18 @@
+<?php
+// require_once('./view/php_connect_root.php');
+require_once('./view/php_connect_h.php');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while ($row = mysqli_fetch_array($result)) {
+    // $escaped_title = htmlspecialchars($row['title']);
+    // $escaped_date = htmlspecialchars($row['created']);
+    // $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
+    if (($row['img_test'])) {
+        $list = $list . "<a class=\"postImg_wrapper\" href=\"index.php?id={$row['id']}\"><img class=\"postImg\" src=\"{$row['img_test']}\"></a>";
+    } else {
+        $list = $list . "<a class=\"postImg_wrapper\" href=\"index.php?id={$row['id']}\"><img class=\"postImg\" src=\"./designSource/kyj/cat.jpg\"></a>";
+        // $list = $list."<a class=\"postImg_wrapper\" href=\"index.php?id={$row['id']}\"><img class=\"postImg\" src=\"http://iamnothalim.dothome.co.kr/designSource/kyj/cat.jpg\"></a>";
+    }
+}
