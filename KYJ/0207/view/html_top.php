8aca45ddb0b9af623bea50d666110534ee6fcee4
KangYeJin <kyjin8@naver.com>
2021-02-07 14:16:46 +0900

    모듈화 수정, 오류 해결
    config.php 통해서 코드들 위에서 connect, login_session 처리
    두번저장 오류 해결한듯 - 재발 시 새로고침 쪽 확인할 것
    로그인 후 글쓰기 했는데 닉네임 안 나타나는 문제 해결

---
+++ KYJ/0207/view/html_top.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>멍냥펀치</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/main_head.css">
+    <link rel="stylesheet" href="./css/main_slide.css">
+    <link rel="stylesheet" href="./css/main_post.css?ver=1">
+    <link rel="stylesheet" href="./css/main_comments.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div id="Wrap" class="wrap_punch">
+        <header id="Head">
+            <div class="service_header top">
+                <div class="wrap_inner">
+                    <div class="f_l">
+                        <button id="btnServiceMenu" type="button" class="f_l text_hide btn_menu img_ico">메뉴</button>
+                    </div>
+                    <div class="f_c">
+                        <img src="designSource/logo2/logo_transparent_cut.png" style="height: 80px; width: 80px;">
+                    </div>
+                    <div class="f_r">
+<?php
+                    if(isset($_SESSION['id'])){
+?>
+                        <!-- <a href="login.html" class="f_r btn_request btn_default">로그인</a> -->
+                        <button class="f_r btn_request btn_default" onclick="logout()">로그아웃</button>
+<?php
+                    }else{
+?>
+                        <a href="login.html" class="f_r btn_request btn_default">로그인</a>
+<?php
+                    }
+?>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapSideMenu"></div>
+        </header>
